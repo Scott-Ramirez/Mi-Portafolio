8df0c919ec5a97150c23a0c73dbc0a9516493b17
Scott-Ramirez <anthonyscottramirez@gmail.com>
2023-03-31 08:35:38 -0500

    js fx

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const proyectos = [
+    {
+      titulo: "SISTEMA DE VENTA DE PASAJES",
+      descripcion:
+        "Para las empresas de transportes de pasajeros via terrestre y fluvial",
+      link: "https://practicascott2023.000webhostapp.com/",
+      fecha: "13/03/2023",
+      imagen: "proyecto1.png",
+      tecnologias: ["HTML","-", "CSS","-","MySQL","-","JAVASCRIPT","-","BOOTSTRAP","-","SWEETALERT","-","DOMPDF","-","AJAX","-","PHP"],
+    },
+  ];
+  
+  const informacionPersonal = {
+    nombre: "Anthony Scott",
+    subtitulo: "Desarrollador web",
+    imagen: "img/MiFoto.png",
+    otros: [
+      ["Nacionalidad", "Peruano"],
+      [
+        "Edad",
+        new Date(new Date() - new Date("2002/02/22")).getFullYear()-1970 + " años",
+      ],
+    ],
+    idiomas: [
+      ["Español", "Nativo"],["Inglés", "Básico"],
+    ],
+    tecnologias: [
+      ["Html", 5],["Css", 5],["Javascript", 3],["Php", 6],["MySql", 5
+      ],["Bootstrap",5],["Java",3],["Python",3],["SQLServer",3]
+    ],
+    redes: [
+      ["github", "https://github.com/Scott-Ramirez"],
+      ["linkedin", "https://www.linkedin.com/in/scottramirez-softwareenginner/"],
+      ["twitter", "https://twitter.com/ScottRa94848220"],
+      ["instagram", ""],
+    ]
+  };
